9d43b5af997a81e093a9ecb91256ece850b76c42
Axel <axel.coudair.pro@gmail.com>
2021-05-24 16:53:17 +0200

    :construction: Placing header text

--- components/Header.js
+++ components/Header.js
@@ -6,12 +6,24 @@ export default function Header() {
     <div>
       <div class="flex">
         <div className="flex-1">
-          <Image
-            src="/images/logo.png"
-            alt="Axel Coudair Logo"
-            width={32}
-            height={32}
-          />
+          <div class="gap-x-4 flex">
+            <div class="row-span-3 ">
+              <Image
+                src="/images/logo.png"
+                alt="Axel Coudair Logo"
+                width={65}
+                height={65}
+              />
+            </div>
+            <div class="grid grid-cols-1 gap-x-4 ">
+              <div class="col-span-2 ">
+                <span>Axel Coudair</span>
+              </div>
+              <div class="row-span-2 col-span-2 ">
+                <span>Développeur Full Stack</span>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="pr-2 place-self-center">
           <Image
