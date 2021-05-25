56f2642096a31bbc7b07fd5ab04ed27401673b9a
Axel <axel.coudair.pro@gmail.com>
2021-05-25 20:00:40 +0200

    🚧 Placing description text

---
+++ components/Description.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Description() {
+  return (
+    <div>
+      <p className="font-manrope">
+        5 ans d’expérience dont 3 ans dans le développement web. Mais également
+        des expériences produit, design et marketing
+      </p>
+      <p className="font-manrope font-bold">
+        24 ans | Paris, France | axel.coudair@gmail.com | +33 6 48 18 06 06
+      </p>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Header from '../components/Header'
 import Layout from '../components/Layout'
-import Image from 'next/image'
+import Description from '../components/Description'
 
 export default function Home() {
   return (
       <Layout title="Axel Coudair | Développeur Full stack">
         <Header className="fixed"></Header>
+        <Description className=""></Description>
+
       </Layout>
   )
 }
