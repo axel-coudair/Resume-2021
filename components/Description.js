8697e639bbbdfc87ab2273608ece201379d3441c
Axel <axel.coudair.pro@gmail.com>
2021-05-25 20:16:11 +0200

    💄 description styling

--- components/Description.js
+++ components/Description.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Description() {
   return (
-    <div>
-      <p className="font-manrope">
+    <div className="max-w-lg py-6">
+      <p className="font-manrope font-light">
         5 ans d’expérience dont 3 ans dans le développement web. Mais également
         des expériences produit, design et marketing
       </p>
-      <p className="font-manrope font-bold">
+      <p className="font-manrope font-bold pt-4">
         24 ans | Paris, France | axel.coudair@gmail.com | +33 6 48 18 06 06
       </p>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
-import Header from '../components/Header'
-import Layout from '../components/Layout'
-import Description from '../components/Description'
+import Header from "../components/Header";
+import Layout from "../components/Layout";
+import Description from "../components/Description";
 
 export default function Home() {
   return (
-      <Layout title="Axel Coudair | Développeur Full stack">
-        <Header className="fixed"></Header>
-        <Description className=""></Description>
-
-      </Layout>
-  )
+    <Layout title="Axel Coudair | Développeur Full stack">
+      <Header className="fixed"></Header>
+      <Description></Description>
+    </Layout>
+  );
 }
