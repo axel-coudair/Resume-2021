cd7003aab964b586aa46d55914347958538c031a
Axel <axel.coudair.pro@gmail.com>
2021-05-26 00:24:27 +0200

    :lipstick: Enhancing style :sparkles: Label for download button

--- components/DatedEvent.js
+++ components/DatedEvent.js
@@ -10,26 +10,24 @@ export default function DatedEvent() {
           <span className="text-xl text-gray-500"> @Infotel</span>
         </h4>
         <h4 className="text-xl text-gray-300 md:hidden">2019 - 2020</h4>
-        <p>
-          <span>
-            Développement de sites vitrines sur CMS (Wordpress, Drupal).{" "}
-          </span>
-          <div className="sm:grid grid-cols-2 gap-4">
-            <ul className="list-disc list-inside">
-              <li>Recherche Utilisateur</li>
-              <li>Architecture de l'information</li>
-              <li>Réalisation d'interviews utilisateurs</li>
-              <li>Test utilisateurs</li>
-            </ul>
-            <ul className="list-disc list-inside">
-              <li>Animation d'ateliers d'idéations</li>
-              <li>Wireframes</li>
-              <li>Maquettes</li>
-              <li>Designer - Design Thinking</li>
-            </ul>
-          </div>
-          <span>Outils : Photoshop, Wordpress, OVH, PHP </span>
-        </p>
+        <span>
+          Développement de sites vitrines sur CMS (Wordpress, Drupal).{" "}
+        </span>
+        <div className="sm:grid grid-cols-2 gap-4">
+          <ul className="list-disc list-inside">
+            <li>Recherche Utilisateur</li>
+            <li>Architecture de l'information</li>
+            <li>Réalisation d'interviews utilisateurs</li>
+            <li>Test utilisateurs</li>
+          </ul>
+          <ul className="list-disc list-inside">
+            <li>Animation d'ateliers d'idéations</li>
+            <li>Wireframes</li>
+            <li>Maquettes</li>
+            <li>Designer - Design Thinking</li>
+          </ul>
+        </div>
+        <span>Outils : Photoshop, Wordpress, OVH, PHP </span>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,33 +2,17 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
-    // screens: {
-    //   sm: '480px',
-    //   md: '768px',
-    //   lg: '976px',
-    //   xl: '1440px',
-    // },
-    // colors: {
-    //   gray: colors.coolGray,
-    //   blue: colors.lightBlue,
-    //   red: colors.rose,
-    //   pink: colors.fuchsia,
-    // },
     fontFamily: {
-      bebasNeue: ["Bebas Neue", 'cursive'],
-      manrope: ["Manrope", 'sans-serif'],
+      bebasNeue: ["Bebas Neue", "cursive"],
+      manrope: ["Manrope", "sans-serif"],
     },
-    // extend: {
-    //   spacing: {
-    //     '128': '32rem',
-    //     '144': '36rem',
-    //   },
-    //   borderRadius: {
-    //     '4xl': '2rem',
-    //   }
-    // }
+    extend: {
+      screens: {
+        print: { raw: "print" },
+      },
+    },
   },
   variants: {
     extend: {},
